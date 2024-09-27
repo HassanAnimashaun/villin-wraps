7e9f09eb790f6f2d51909adce96801cdbf4be627
HassanAnimashaun <hassananimashaun@outlook.com>
2024-09-27 01:33:41 -0400

    adding input fields for form

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,7 @@
 const openBtn = document.querySelector('#open-cta');
+const form = document.querySelector('#form-container');
 
 openBtn.addEventListener('click', () => {
     openBtn.style.display = 'none';
+    form.style.display = 'flex';
 })
